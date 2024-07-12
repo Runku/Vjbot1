c9120efb1b4f5679012fd3900544d1532592dbd7
Runku <143589139+Runku@users.noreply.github.com>
2024-07-12 09:35:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/BackupByJaggiiRequest')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/BackupByJaggii')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/+buYbWz4TT_NiMTI9')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/BackupByJaggiiRequest') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'BackupByJaggiiRequest') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
